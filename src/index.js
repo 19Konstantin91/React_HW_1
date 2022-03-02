5a9c82a985cf28bb85b77e6e256e4c9e1ef82592
19Konstantin91 <kostya_klpov@mail.ru>
2022-03-03 00:16:15 +0300

    hw1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App.js';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+export function Message(props) {
+  return (
+    <div className="Message">
+      <div className="Message-text">{props.text}</div>
+      <div className="Message-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+function formatDate(date) {
+  return date.toLocaleString();
+}
+
+const message = {
+  text: 'Hello React!',
+  date: new Date(),
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Message
+        text={message.text}
+        date={message.date}
+      />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
