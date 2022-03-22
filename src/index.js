c5cf12a0ea0ac51a41a7cbea423ddf8ddfa5dcde
19Konstantin91 <kostya_klpov@mail.ru>
2022-03-22 23:40:00 +0300

    hw2

--- src/index.js
+++ src/index.js
@@ -1,36 +1,72 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App.js';
 
 import './index.css';
 
+const MessgeList = () => {
+    const [value, setValue] = useState ("");
+    const [messages, setmessages] = useState ([
+        {
+            authore: "Bot",
+            message: "message1",
+            date: new Date(). toLocaleDateString(),
+        },
+    ]);
+
+    const sendMessage =() => {
+        setmessages ([...messages,{author: "User", message : value}])
+    };
+
+    useEffect ( () => {
+        const LastMessages = messages [massages.lenght -1];
+        let timerId = null;
+
+        if (messages.length && LastMessages.author == "User"){
+            timerId = setTimeout ( () => {
+                setmessages ([
+                    ...messages,
+                    {author: "Bot", message: "Hello from bot"},
+                ]);
+            }, 1500);
+        };
+
+        return () => {
+            clearInterval (timerId);
+        };
+    }, [messages]);
 
 
-// export function Message(props) {
-//   return (
-//     <div className="Message">
-//       <div className="Message-text">{props.text}</div>
-//       <div className="Message-date">{formatDate(props.date)}</div>
-//     </div>
-//   );
-// }
+    return (
+        <div>
+            <input
+            placeholder='введите сообщение...'
+            value = {value}
+            onChange = {(event) => setValue (event.target.value)}
+            />
+            <button onClick = {sendMessage}>send message</button>
+            <hr />
+            {messages.map ((message) => (
+                <div>
+                    <h2> {message.author} </h2>
+                    <p> {massage.message} </p>
+                    <hr />
+                <div />    
+            ))}
+        </div>
+    )
+},
 
-// function formatDate(date) {
-//   return date.toLocaleString();
-// }
+const App = () => {
+    return (
+        <>
+         <MessgeList />
+        </>
+    );
+},
 
-// const message = {
-//   text: 'Hello React!',
-//   date: new Date(),
-// };
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//     <Message
-//         text={message.text}
-//         date={message.date}
-//       />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementbyId("root")
+),
