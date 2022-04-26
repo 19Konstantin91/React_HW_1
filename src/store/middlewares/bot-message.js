8fbe8fde6b2e606f6c3d8f9984a8c423c2d6d9c3
19Konstantin91 <kostya_klpov@mail.ru>
2022-04-26 21:11:11 +0300

    modified

--- src/store/middlewares/bot-message.js
+++ src/store/middlewares/bot-message.js
@@ -1,12 +1,19 @@
 import { SEND_MESSAGE, sendMessage } from "../messages";
 
 export const botMessage = (store) => (next) => (action) => {
+  if (
+    action.type === SEND_MESSAGE &&
+    action.payload.message.author === "User"
+  ) {
+    setTimeout(() => {
+      store.dispatch(
+        sendMessage(action.payload.roomId, {
+          author: "Bot",
+          message: "Middleware bot!",
+        })
+      );
+    }, 1500);
+  }
 
-    if (action.type === SEND_MESSAGE && action.payload.message.author === "User") {
-       setTimeout ( () => {
-        store.dispatch(sendMessage(action.payload.roomId, {author: "Bot", message: "Middleware bot!"}))
-       }, 1500);
-    }
-
-    return next(action);
-}
+  return next(action);
+};
